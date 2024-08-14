5574635cf3a8ab745ccf19a553aa35df096e190a
Xarblu <xarblu@protonmail.com>
2024-08-14 23:13:53 +0200

    muxer setting + fixes

--- movmux.py
+++ movmux.py
@@ -13,7 +13,7 @@ class TrackNotFoundError(Exception):
 class MediaFile():
     def __init__(self, file: str):
         p = subprocess.run(["mediainfo", "--version"], capture_output=True, check=True, text=True)
-        print(f"MediaFile using {" ".join(p.stdout.splitlines())}", file=sys.stderr)
+        print(f"MediaFile: Using {" ".join(p.stdout.splitlines())}", file=sys.stderr)
 
         if not file:
             self.file = ""
@@ -54,12 +54,16 @@ class MediaFile():
             if channels > maxchannels:
                 maxchannels = channels
 
-        tracks = [track for track in tracks if int(track["Channels"]) == maxchannels]
+        found = [track for track in tracks if int(track["Channels"]) == maxchannels]
+        if found:
+            tracks = found
         if len(tracks) == 1:
             return tracks[0]
 
         # select lossless audio
-        tracks = [track for track in tracks if track["Compression_Mode"] == "Lossless"]
+        found = [track for track in tracks if track["Compression_Mode"] == "Lossless"]
+        if found:
+            tracks = found
         if len(tracks) == 1:
             return tracks[0]
 
@@ -72,7 +76,9 @@ class MediaFile():
             if bitrate > maxbitrate:
                 maxbitrate = bitrate
 
-        tracks = [track for track in tracks if int(track["BitRate"].split("/")[0]) == maxbitrate]
+        found = [track for track in tracks if int(track["BitRate"].split("/")[0]) == maxbitrate]
+        if found:
+            tracks = found
         if len(tracks) == 1:
             return tracks[0]
 
@@ -98,13 +104,21 @@ class MediaFile():
         try:
             return tracks[id]
         except IndexError:
-            msg = f"Couldn't find track {id}" + f"of type {tracktype}" if tracktype else ""
+            msg = f"Couldn't find track {id}" + (f" of type {tracktype}" if tracktype else "")
             raise TrackNotFoundError(msg)
 
 class Muxer():
-    def __init__(self, infile: str, outfile: str):
-        p = subprocess.run(["mkvmerge", "--version"], capture_output=True, check=True, text=True)
-        print(f"Muxer using {" ".join(p.stdout.splitlines())}", file=sys.stderr)
+    def __init__(self, infile: str, outfile: str, implementation: str = "mkvmerge"):
+        if implementation == "mkvmerge":
+            p = subprocess.run(["mkvmerge", "--version"], capture_output=True, check=True, text=True)
+            print(f"Muxer: Using {p.stdout.strip()}", file=sys.stderr)
+            self.impl = implementation
+        elif implementation == "ffmpeg":
+            p = subprocess.run(["ffmpeg", "-version"], capture_output=True, check=True, text=True)
+            print(f"Muxer: Using {p.stdout.splitlines()[0].strip()}", file=sys.stderr)
+            self.impl = implementation
+        else:
+            raise ValueError(f"Unsopported implementation: {implementation}")
 
         if os.path.isfile(outfile):
             raise RuntimeError(f"Output file {outfile} exists")
@@ -184,12 +198,12 @@ class Muxer():
                 return
 
         try:
-            self.audiotracks.append(self.infile.getBestAudioTrack(language=language))
+            self.addTrack(id=self.infile.getBestAudioTrack(language=language)["StreamOrder"])
         except TrackNotFoundError:
             if not optional:
                 raise TrackNotFoundError(f"Could not find required track for language {language}")
 
-    def start(self, pretend: bool = False):
+    def getMkvmergeCmd(self):
         # ensure a video stream exists, if not select the first
         videotracks = self.videotracks if self.videotracks else [self.infile.getTrack(id=0, tracktype="Video")]
 
@@ -233,16 +247,75 @@ class Muxer():
         else:
             cmd += ["--no-subtitles"]
 
-        # setup default and forced flags
+        # setup other flags
         for track in videotracks + audiotracks + texttracks:
             cmd += ["--default-track-flag",
                     track["StreamOrder"] + (":1" if track["Default"] == "Yes" else ":0")]
             cmd += ["--forced-display-flag",
                     track["StreamOrder"] + (":1" if track["Forced"] == "Yes" else ":0")]
+            #cmd += ["--compression", track["StreamOrder"] + ":none"]
 
         # add file
         cmd += [self.infile.file]
 
+        return cmd
+
+    def getFfmpegCmd(self):
+        # ensure a video stream exists, if not select the first
+        videotracks = self.videotracks if self.videotracks else [self.infile.getTrack(id=0, tracktype="Video")]
+
+        audiotracks = self.audiotracks
+        texttracks = self.texttracks
+
+        # order of tracks in output file
+        trackorder = []
+        for track in videotracks + audiotracks + texttracks:
+            # for now we only support 1 infile
+            trackorder += ["0:" + track["StreamOrder"]]
+
+        # init cmd
+        cmd = ["ffmpeg", "-analyzeduration", "200M", "-probesize", "1G"]
+        cmd += ["-i", self.infile.file]
+
+        # we only mux
+        cmd += ["-c", "copy"]
+
+        # setup tracks
+        outid = 0
+        for track in videotracks + audiotracks + texttracks:
+            # mapping
+            cmd += ["-map", "0:" + track["StreamOrder"]]
+
+            # disposition tags
+            cmd += ["-disposition:" + str(outid)]
+            disposition = []
+            if track["Default"] == "Yes":
+                disposition += ["default"]
+            if track["Forced"] == "Yes":
+                disposition += ["forced"]
+            if disposition:
+                cmd += ["+".join(disposition)]
+            else:
+                cmd += ["0"]
+
+            outid += 1
+
+        # copy timestamps (avoids audio desync)
+        cmd += ["-start_at_zero", "-copyts", "-avoid_negative_ts", "disabled"]
+
+        # add outfile
+        cmd += [self.outfile]
+
+        return cmd
+
+    def start(self, pretend: bool = False):
+        if self.impl == "mkvmerge":
+            cmd = self.getMkvmergeCmd()
+        elif self.impl == "ffmpeg":
+            cmd = self.getFfmpegCmd()
+        else:
+            # should not be reachable but eh
+            raise RuntimeError()
         # print and run
         print("CMD: " + str(cmd))
         if pretend:
@@ -264,6 +337,8 @@ def parse_args():
                         help = "Manual track selection - global id, type id (<type>:id), optional suffix '!' - mark 'forced'")
     parser.add_argument("--pretend", const = True, default = False, action = "store_const",
                         help = "Print commands instead of executing")
+    parser.add_argument("--muxer", type = str, nargs = 1,
+                        help = "Muxer implementation (mkvmerge or ffmpeg)")
     args = parser.parse_args()
 
     return {
@@ -271,14 +346,15 @@ def parse_args():
             "outfile": args.outfile[0],
             "langs": args.langs[0].split() if args.langs else [],
             "tracks": args.tracks[0].split() if args.tracks else [],
-            "pretend": args.pretend
+            "pretend": args.pretend,
+            "muxer": args.muxer[0] if args.muxer else "ffmpeg"
             }
         
 def main():
     args = parse_args()
 
     # the main muxer object
-    muxer = Muxer(args["infile"], args["outfile"])
+    muxer = Muxer(args["infile"], args["outfile"], args["muxer"])
 
     # add manual tracks first
     for track in args["tracks"]:
